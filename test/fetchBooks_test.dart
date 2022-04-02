120e60edfa8463702348f520295d4c82994a3b95
Lukáš Novoroľník <lukas.novorolnik@gmail.com>
2022-04-02 21:32:57 +0200

    test for fetchBooks (creating list of Book)

--- test/fetchBooks_test.dart
+++ test/fetchBooks_test.dart
@@ -17,5 +17,35 @@ void main() {
       final Book book = await APIProvider.api.fetchBookDetails('9780321812186');
       expect(book.title, 'Effective JavaScript');
     });
+
+    test('Create list from api call', () async {
+      final mockHttpClient = MockClient((request) async {
+        final mapJson = {
+          'books': [
+            {
+              "title": "Effective JavaScript",
+              "subtitle": "68 Specific Ways to Harness the Power of JavaScript",
+              "isbn13": "9780321812186",
+              "price": "\$21.99",
+              "image": "https://itbook.store/img/books/9780321812186.png",
+              "url": "https://itbook.store/books/9780321812186"
+            },
+            {
+              "title": "Learning JavaScript",
+              "subtitle":
+                  "A Hands-On Guide to the Fundamentals of Modern JavaScript",
+              "isbn13": "9780321832740",
+              "price": "\$8.99",
+              "image": "https://itbook.store/img/books/9780321832740.png",
+              "url": "https://itbook.store/books/9780321832740"
+            },
+          ]
+        };
+        return Response(json.encode(mapJson), 200);
+      });
+
+      final List<Book> bookList = await APIProvider.api.fetchBooks('java');
+      expect(bookList.isNotEmpty, true);
+    });
   });
 }
